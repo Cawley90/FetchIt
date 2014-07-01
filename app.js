7aca36928ffa9b02e16a25b5c62fec28c793a68f
Cawley90 <chrisjam90@gmail.com>
2014-06-30 21:19:58 -0700

    Interaction
    Searchbox and submit

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+$(document).ready(function(){
+var userInput = $('input[id=vidSearch]').val();
+
 // Your use of the YouTube API must comply with the Terms of Service:
 // https://developers.google.com/youtube/terms
 
@@ -21,11 +24,11 @@ function onYouTubeApiLoad() {
     search();
 }
 
-function search() {
+$(document).on('click', '#searchSubmit', function search() {
     // Use the JavaScript client library to create a search.list() API call.
     var request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: 'Bungie',
+        part: snippet,
+        q: userInput,
     });
     
     // Send the request to the API server,
@@ -37,3 +40,5 @@ function search() {
 function onSearchResponse(response) {
     showResponse(response);
 }
+
+});
