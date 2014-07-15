137a916b20230265c363c48f0e4fe9f4a9be58d2
Cawley90 <chrisjam90@gmail.com>
2014-07-14 18:26:58 -0700

    BeforeOverhaul
    Updating the way the video box loads.

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 
 $(document).ready(function() {
     
-    $('#userinput').keyup(function() {
+    $('#userinput')on.keyup(function() {
         var userSearch = $(this).val();
         var searchCri = encodeURIComponent(userSearch);
         var ytDirect = 'http://gdata.youtube.com/feeds/api/videos?q='+searchCri+'&format=5&max-results=1&v=2&alt=jsonc';
         //var ytvid = 'https://www.googleapis.com/youtube/v3/videos?q='+searchCri+'&maxResults=1&key={AIzaSyA0-TyKv764oZyZ9gIPuagu28t-Z5nCL-k}';
+
     
 
         $.ajax ({
@@ -19,9 +20,9 @@ $(document).ready(function() {
                         var videoId = data.id;
                         var videoTitle = data.title;
                         var videoHits = data.viewCount;
-                        var vidbox = "<iframe width='640' height='385' src='http://www.youtube.com/embed/"+videoId+"'frameborder='0' type='text/html'></iframe>";
-                        var vidResult = "<div id = 'result'>"+vidbox+"</div>";
-                        $('#result').html(vidResult);
+                        var vidbox = "<iframe width='560' height='3' src='http://www.youtube.com/embed/"+videoId+"'frameborder='0' type='text/html'></iframe>";
+                        var result = "<div id = 'result'>"+vidbox+"</div>";
+                        $('#result').html(result);
                     });
                 }
 
